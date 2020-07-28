431e613a74776007902e6b12b99f16203a597932
Daniel Marcano <dmarcanor2@gmail.com>
2020-07-28 14:48:34 -0400

    Setting declare strict v2

--- src/ReviewSys/Review/Domain/ReviewRepository.php
+++ src/ReviewSys/Review/Domain/ReviewRepository.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Practice\ReviewSys\Review\Domain;
 
 interface ReviewRepository
--- src/ReviewSys/Shared/Command/CommandHandler.php
+++ src/ReviewSys/Shared/Command/CommandHandler.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Practice\ReviewSys\Shared\Command;
 
 interface CommandHandler
